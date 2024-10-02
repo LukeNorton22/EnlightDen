056730602c80514fa8b08e0f45e2af2a3a880b0f
Luke Norton <luke.norton@selu.edu>
2024-10-02 14:55:55 -0500

    added dash navigation when clicking logo

--- enlightden-frontend/src/components/Navbar.tsx
+++ enlightden-frontend/src/components/Navbar.tsx
@@ -1,14 +1,16 @@
-// NavBar.tsx
 import React from 'react';
 import { Menu, Container, Icon } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 const NavBar: React.FC = () => {
   return (
     <Menu inverted fixed="top" style={{ backgroundColor: '#2E2E3E', height: '60px', border: 'none' }}>
       <Container>
         <Menu.Item header style={{ color: '#00B5D8', fontSize: '1.5em' }}>
-          <Icon name="graduation" />
-          EnlightDen
+          <Link to="/dash" style={{ color: '#00B5D8', textDecoration: 'none' }}>
+            <Icon name="graduation" />
+            EnlightDen
+          </Link>
         </Menu.Item>
         <Menu.Item position="right" style={{ color: '#B0B0B0' }}>
           <Icon name="user" />
