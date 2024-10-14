4d31b9f6fa503148611577ee568e0499e0d5d652
Robert Turner <mason.robertmason.robert1@gmail.com>
2024-10-14 18:12:23 -0500

    .

--- enlightden-frontend/src/apiClient.ts
+++ enlightden-frontend/src/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://localhost:5000', // Your backend's base URL
+  baseURL: 'http://localhost:5203', // Your backend's base URL
   headers: {
     'Content-Type': 'application/json',
   },
